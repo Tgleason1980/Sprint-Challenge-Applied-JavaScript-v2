b16d1eea4885130bae520ff8b8227d0d1197e27e
Tgleason1980 <terryscoding@gmail.com>
2019-07-14 18:15:21 -0500

    mvp

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -6,7 +6,7 @@
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
 //   <div class="headline">{Headline of article}</div>
-//   <div class="author">
+//    <div class="author">
 //     <div class="img-container">
 //       <img src={url of authors image} />
 //     </div>
@@ -16,3 +16,41 @@
 //
 // Create a card for each of the articles and add the card to the DOM.
 
+
+axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(res => {
+    Object.keys(res.data.articles).forEach(topic=>{
+      res.data.articles[topic].forEach(article=>{
+        document.querySelector('.cards-container').appendChild(Card(article));
+      });
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+
+function Card(data) {
+    const card = document.createElement('div')
+    const headline = document.createElement('div')
+    const author = document.createElement('div')
+    const photoContainer = document.createElement('div')
+    const authorPhoto = document.createElement('img')
+    const authorName = document.createElement('span')
+
+  card.classList.add('card');
+  headline.classList.add('headline');
+  author.classList.add('author');
+  photoContainer.classList.add('img-container');
+
+  headline.textContent = data.headline;
+  authorName.textContent = "By " + data.authorName;
+
+  authorPhoto.src = data.authorPhoto;
+
+  photoContainer.appendChild(authorPhoto);
+  author.append(photoContainer, authorName);
+  card.append(headline, author);
+
+  return card;
+}
